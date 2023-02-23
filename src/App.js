35042107d7ce465188d9765ea17cf39c1353d86f
Daniel Schmitz <danieljfa@gmail.com>
2023-02-23 12:33:03 -0300

    clear+css+dark mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import '@picocss/pico'
 
-function App() {
+export default function App() {
+
+  const setTheme = (theme) => {
+    localStorage.setItem('theme', theme)
+    document.documentElement.setAttribute('data-theme', theme)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <nav>
+        <ul>
+          <li><strong>My Books</strong></li>
+        </ul>
+        <ul>
+          <li><a href="#">Categories</a></li>
+          <li><a href="#">Friends</a></li>
+          <li><a href="#" role="button">New Book</a></li>
+        </ul>
+      </nav>
+      <main>
+        <article>
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Pages</th>
+                <th>Description</th>
+                <th>Category</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>The Catcher in the Rye</td>
+                <td>J.D. Salinger</td>
+                <td>375</td>
+                <td>A novel by J.D. Salinger</td>
+                <td>Fiction</td>
+              </tr>
+              <tr>
+                <td>The Catcher in the Rye</td>
+                <td>J.D. Salinger</td>
+                <td>375</td>
+                <td>A novel by J.D. Salinger</td>
+                <td>Fiction</td>
+              </tr>
+              <tr>
+                <td>The Catcher in the Rye</td>
+                <td>J.D. Salinger</td>
+                <td>375</td>
+                <td>A novel by J.D. Salinger</td>
+                <td>Fiction</td>
+              </tr>
+            </tbody>
+          </table>
+        </article>
+      </main>
+
+      <div style={{ textAlign: 'center' }}>
+          <a href="#" onClick={() => { setTheme('dark') }}>Dark</a>
+          &nbsp;&nbsp;
+          <a href="#" onClick={() => { setTheme('light') }}>Light</a>
+      </div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
